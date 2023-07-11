09747a51661999c8b6f7d6fa7b4119494056468e
Madeline King <madeline.king330@gmail.com>
2023-07-10 20:25:28 -0700

    establish db connection between docker containers

---
+++ server/db.ts
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const database = new Pool({
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+  host: process.env.POSTGRES_HOST, /* db container */
+  idleTimeoutMillis: 0,
+  connectionTimeoutMillis: 0,
+});
+
+module.exports = database;
---
+++ server/seed.ts
@@ -0,0 +1,19 @@
+// -- TEST THAT DB CONNECTION HAS BEEN ESTABLISHED
+const db = require('./db');
+
+async function testConnection() {
+  try {
+    const client = await db.connect();
+
+    const result = await client.query('SELECT NOW()');
+    console.log('Connected to the db:', result.rows[0].now);
+
+    client.release();
+  } catch (error) {
+    console.error('Error connecting to the db:', error);
+  } finally {
+    db.end();
+  }
+}
+
+testConnection();
