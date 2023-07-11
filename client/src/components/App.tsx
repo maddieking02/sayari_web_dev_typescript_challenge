0a6b653abad9c9016482156c0fa6f6899416edd4
Madeline King <madeline.king330@gmail.com>
2023-07-10 19:56:18 -0700

    set up basic client side files

---
+++ client/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import bgImage from "../assets/pink-blue-bg.png";
+
+const App = (): React.JSX.Element => {
+  return (
+    <div>
+      <h1>Test</h1>
+      <img src={bgImage} alt="" />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,40 @@
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/src");
+var DIST_DIR = path.join(__dirname, "/dist");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR,
+    assetModuleFilename: "imgs/[name][ext]",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
+        type: "asset",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: DIST_DIR,
+    },
+    host: "0.0.0.0",
+    port: 8080,
+    open: true,
+  },
+};
