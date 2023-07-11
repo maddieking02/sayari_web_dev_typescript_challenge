ddd60e22562910fb47fa21a641bed8c02a3066eb
Madeline King <madeline.king330@gmail.com>
2023-07-10 20:04:48 -0700

    set up basic server side files

---
+++ server/index.ts
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const router = require("express").Router();
+const path = require("path");
+const PORT = process.env.PORT;
+
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
