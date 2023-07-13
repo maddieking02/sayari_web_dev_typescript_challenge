429ed66a9a48d21ed769c1e9f1279af6e8656bb7
Madeline King <madeline.king330@gmail.com>
2023-07-13 06:33:19 -0700

    add simple css styling

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -4,9 +4,24 @@ import bg from "../assets/background.png";
 export const Home = (): React.JSX.Element => {
 
   return (
-    <div id="home-contain" className="center-items-row">
-      <p id="description">Elevate your<br/>programming skills<br/>with stack overfaux</p>
-      {/* <img className="bg" src={bg} alt=""/> */}
+    <div /*id="home-contain" className="center-items-row"*/>
+      <div className="home-context">
+        <p id="description">Elevate your<br/>programming skills<br/>with stack overfaux</p>
+      </div>
+      <div className="area" >
+        <ul className="shapes">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+    </div >
     </div>
 
   );
