096440cf0dfdc1b5d1738a9ecdc4a7b15fc69b88
Madeline King <madeline.king330@gmail.com>
2023-07-13 00:29:37 -0700

    add commentary to files

--- server/index.ts
+++ server/index.ts
@@ -4,10 +4,11 @@ const router = require("express").Router();
 const path = require("path");
 const { controllers } = require('./api/controllers');
 const PORT = process.env.PORT;
-var cors = require('cors')
+const cors = require('cors')
 
 const app = express();
 
+// -- ENABLE CORS FOR DOCKER
 app.use(cors())
 app.use(express.json());
 app.use(router);
@@ -17,6 +18,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
+// -- SERVER ROUTES
 router.get('/results/:search?', controllers.getResults)
 router.get('/posts/:post_id?', controllers.getPost)
 
